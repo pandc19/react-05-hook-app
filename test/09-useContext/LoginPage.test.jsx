2f994878236eaac1a0dd9b56c4e81b2227215fa2
Jonathan Pillajo <jonathan.pillajo@hotmail.com>
2023-09-29 07:32:55 -0500

    Fin Pruebas unitarias y de integración

---
+++ test/09-useContext/LoginPage.test.jsx
@@ -0,0 +1,41 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { UserContext } from '../../src/09-useContext/context/UserContext';
+import { LoginPage } from '../../src/09-useContext/LoginPage';
+
+describe('Pruebas en <LoginPage />', () => {
+
+    test('debe de mostrar el componente sin el usuario', () => {
+
+        render(
+            <UserContext.Provider value={{ user: null }}>
+                <LoginPage />
+            </UserContext.Provider>
+        );
+
+        const preTag = screen.getByLabelText('pre');
+        expect(preTag.innerHTML).toBe('null');
+
+        // screen.debug();
+
+    });
+
+    test('debe de llamar el setUser cuando se hace click en  botón', () => {
+
+        const setUserMock = jest.fn();
+
+        render(
+            <UserContext.Provider value={{ user: null, setUser: setUserMock }}>
+                <LoginPage />
+            </UserContext.Provider>
+        );
+
+        const button = screen.getByRole('button');
+        fireEvent.click(button);
+
+        expect(setUserMock).toHaveBeenCalledWith({
+            email: "jpp@mail.com",
+            id: 123,
+            name: "Jonathan",
+        });
+    });
+});
---
+++ test/09-useContext/MainApp.test.jsx
@@ -0,0 +1,34 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { UserContext } from '../../src/09-useContext/context/UserContext';
+import { MainApp } from '../../src/09-useContext/MainApp';
+import { MemoryRouter } from 'react-router-dom';
+
+
+describe('Pruebas en <MainApp />', () => {
+
+    test('debe de mostrar el HomePage', () => {
+
+        render(
+            <MemoryRouter>
+                <MainApp />
+
+            </MemoryRouter>
+        );
+
+        expect(screen.getByText('HomePage')).toBeTruthy();
+        // screen.debug();
+    });
+
+    test('debe de mostrar el LoginPage', () => {
+
+        render(
+            <MemoryRouter initialEntries={['/login']}>
+                <MainApp />
+
+            </MemoryRouter>
+        );
+
+        expect(screen.getByText('LoginPage')).toBeTruthy();
+        // screen.debug();
+    });
+});
